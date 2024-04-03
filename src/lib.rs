01d6f814cc1d109e6f315d5a204f82af04a95843
takesei <sei06k14@gmail.com>
2024-04-03 17:03:50 +0900

    :sparkles: add search

--- src/lib.rs
+++ src/lib.rs
@@ -30,8 +30,20 @@ impl<'a> Config<'a> {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result: Vec<&'a str> = vec![];
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod test {
+    use crate::search;
+
     #[test]
     fn one_result() {
         let query: &str = "duct";
